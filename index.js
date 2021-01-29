a4183ed31daabb411b789a4543d80cb8a087ac4b
Raushan <raushan.043.kumar@gmail.com>
2021-01-30 00:43:26 +0530

    previous problem solved

--- index.js
+++ index.js
@@ -1,99 +1,182 @@
-const git_link = "https://api.github.com/users/";
-const container = document.querySelector('.container');
-const contents =document.querySelector('.content');
-const main = document.querySelector('.main');
-const form= document.querySelector('#form');
-const input= document.querySelector('#text');
+const git_links = "https://api.github.com/users/";
+// container.innerHTML='';
+const container= document.querySelector('.container')
+const beformain= document.querySelector('.beforemain');
+const heading = document.querySelector('.heading')
+const reload =document.querySelector('.reload');
+  reload.addEventListener('click',((e)=>{
+      location.reload();
+  }))
+var git_link;
+const form = document.querySelector('.form');
+// console.log(container);
+const text = document.querySelector('.text');
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
 })
+text.addEventListener('change',(e)=>{
+    console.log(e.target.value);
+git_link= git_links+e.target.value;
+console.log(git_link);
 
-input.addEventListener('change',async(e)=>{
-    // console.log(e.target.value);
-    // contents.innerHTML='';
-    // main.innerHTML='';
-  var git_links;
+
+  const finaldata= justcheck(git_link);
+// var result;
+// console.log(finaldata);
+finaldata.then((e)=>{
+    if(e=="right")
+    {
+        getdata();
+        text.value='';
+beformain.classList.add('hidden');
+container.classList.remove('gate');
+    }
+    else
+    {
+        heading.innerHTML="Please Enter the correct user name <br> So that i could fetch your data from git hub <br> <span> Please try again<span>"
+    }
+})
+// console.log(result);
+//  const final= 
+
+// console.log(final);
+
+})
+
+const upper = document.createElement('div')
+upper.setAttribute('class','upper');
+container.insertAdjacentElement('afterbegin',upper);
+
+
+const lower = document.createElement('div')
+lower.setAttribute('class','lower');
+container.insertAdjacentElement('beforeEnd',lower);
+// var html1 =`<hr class="divider">`;
+
+function dividerline()
+{
+    const hr = document.createElement('hr');
+hr.setAttribute('class','divider');
+lower.insertAdjacentElement('beforebegin',hr);
+
+}
+
+
+
+async function getdata(link)
+{
     try {
-        const additional= e.target.value;
-        git_links=git_link+additional;
-        console.log(git_links);
-        input.value='';
-const git = async () => {
+        const fetchs =await fetch(git_link);
+        // console.log(fetchs);
+        const fetchsdata = await fetchs.json();
+        console.log(fetchsdata);
+        const fetch1=await setdata(fetchsdata);
+        await setbutton(fetchsdata.repos_url);
+        // return fetch1;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+
+
+
+async function setdata(data)
+{
+    // console.log(data);
+    const times =  new Date(data.created_at);
+    const day= times.getDate();
+    const month=times.getMonth();
+    const year= times.getFullYear();
+    const value= `${`${day<10? `0${day}`: `${day}`}`}/${`${month<10? `0${month}`: `${month}`}`}/${year}`
+    // console.log(value);
+    var html =`
+    <div class="image">
+        <img src=${data.avatar_url}>
+    </div>
+    <div class="bio">
+     
+        <div class="name">${data.name}</div>
+        <div class="about">${data.bio? data.bio:''}</div>
+        <div class="repos"><span> Public Repos: <span>${data.public_repos}</div>
+        <div class="created"> <span> Created at: </span>${value}</div>
+    </div>
+`
+upper.innerHTML= html;
+}
+
+
+async function setbutton(data)
+{
     try {
-        const data = await fetch(git_links);
-        const data_fetch = await data.json();
-        // console.log(data_fetch);
-        return data_fetch;
+        const fetchs = await fetch(data);
+        const fetchsdata = await fetchs.json();
+        // console.log(fetchsdata);
+        const selfmades = await selfmade(fetchsdata);
+        // console.log("raushan");
+        // console.log(selfmades);
+        stickbutton(selfmades);
+        
     } catch (error) {
         console.log(error);
     }
 }
-const data = git();
-data.then((e) => {
-    // console.log(e.repos_url);
-    return e;
-}).then(async (e) => {
+
+async function selfmade(data)
+{
+    // console.log(data);
     try {
-        const data = await fetch(e.repos_url);
-        const data_fetch = await data.json();
-        //  console.log(data_fetch);
-        const real = data_fetch.filter((element, index) => {
-            if (element.fork != true) {
-                // console.log(e.name);
+        
+      const filters=  await data.filter( (element)=>
+        {
+            if(!element.fork)
+            {
+                // console.log(element);
                 return element;
             }
         })
-        // console.log(real);
-        return { real, e };
+        // console.log('raushan');
+        // console.log(filters);
+        return filters;
     } catch (error) {
         console.log(error);
     }
-}).then((content) => {
-    const details = content.e;
-    // console.log(details);
-    const main = document.createElement('div');
-    const name_div = document.createElement('div');
-    const image_div = document.createElement('div');
-    image_div.setAttribute('class','image');
-    name_div.setAttribute('class','details');
-    main.setAttribute('class', 'projects');
-    contents.insertAdjacentElement('beforeend', main);
-    // console.log(content.real);
-    // main.style.background="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png"
-    const cssTemplateString = `.main::after{background: url("${details.avatar_url}")}`;
-    const styleTag = document.createElement("style");
-      styleTag.innerHTML = cssTemplateString;
-     document.head.insertAdjacentElement('afterEnd', styleTag);
-    const html1=`
-    <h1 class="name"><span class="heading">Name:&nbsp;</span>${details.name}</h1>
-    <h1 class="bio"><span class="heading">Bio:&nbsp;</span> ${details.bio}</h1>
-`;
-const html2=`
-<img src=${details.avatar_url};
-    alt="sorry your image not availabe in the git hub profile" srcset="">
-`
-image_div.innerHTML=html2;
-// contents.insertAdjacentElement('beforebegin',image_div);
-name_div.innerHTML=html1;
-contents.insertAdjacentElement('afterbegin',name_div);
-var html='';
-    content.real.forEach(element => {
 
-        html += `
-        <a href=${element.html_url}>
-    <button>${element.name}</button>
-</a>
-        `
-    });
-    main.innerHTML= html;
-    // console.log(html);
-})
-    } catch (error) {
-        console.log(error);
-    }
-    // location.reload();
+    
 }
-)
 
+async function stickbutton(data)
+{
+    // console.log(data);
+    var html1='';
+   data.forEach(element => {
+    //    console.log(element);
+     html1 += `
+          <button class="btn">${element.name}</button>
+`
+   });
+   lower.innerHTML= html1;
+}
+dividerline();
 
+async function justcheck(data)
+{
+    try {
+        const fectdata= await fetch(data);
+        const fectdatas= await fectdata.json();
+        // console.log(fectdatas);
+        if (fectdatas.name) {
+            return('right');
+        }
+        else{
+            return("wrong");
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
 
+    // return "raushan"
+}
